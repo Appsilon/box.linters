f62d48ac59044939b0eb140d6836f89b8357d86a
Rodrigo Basa <rodrigo@appsilon.com>
2024-04-30 11:05:28 +0800

    can now check for unused imported functions from modules

--- tests/testthat/test-linter_box_unused_attached_pkg_fun_linter.R
+++ tests/testthat/test-linter_box_unused_attached_pkg_fun_linter.R
@@ -1,96 +1,64 @@
-test_that("box_unused_attached_pkg_fun_linter skips used box-attached functions.", {
-  linter <- box_unused_attached_pkg_fun_linter()
+test_that("box_unused_attached_mod_fun_linter skips used box-attached functions.", {
+  linter <- box_unused_attached_mod_fun_linter()
 
   good_box_usage <- "box::use(
-    fs[dir_ls, path_file],
-    shiny[...],
-    stringr
+    path/to/module_a[a_fun_a, a_fun_b],
+    path/to/module_b[b_fun_b],
   )
 
-  box::use(
-    path/to/module1,
-    path/to/module2[a, b, c],
-    path/to/module3[...]
-  )
-
-  dir_ls('path')
-  path_file('path/to/file')
-  stringr$str_sub('text', 1, 2)
-  is.reactive(x)
+  a_fun_a()
+  a_fun_b()
+  b_fun_b()
   "
 
   lintr::expect_lint(good_box_usage, NULL, linter)
 })
 
-test_that("box_unused_attached_pkg_fun_linter skips used box-attached aliased functions.", {
-  linter <- box_unused_attached_pkg_fun_linter()
+test_that("box_unused_attached_mod_fun_linter skips used box-attached aliased functions.", {
+  linter <- box_unused_attached_mod_fun_linter()
 
   good_box_usage <- "box::use(
-    fs[fun_alias = dir_ls, path_file],
-    shiny[...],
-    stringr
+    path/to/module_a[a_fun_a, fun_alias = a_fun_b],
+    path/to/module_b[gun_alias = b_fun_b],
   )
 
-  box::use(
-    path/to/module1,
-    path/to/module2[a, b, c],
-    path/to/module3[...]
-  )
-
-  fun_alias('path')
-  path_file('path/to/file')
-  stringr$str_sub('text', 1, 2)
-  is.reactive(x)
+  a_fun_a()
+  fun_alias()
+  gun_alias()
   "
 
   lintr::expect_lint(good_box_usage, NULL, linter)
 })
 
-test_that("box_unused_attached_pkg_fun_linter blocks box-attached functions unused.", {
-  linter <- box_unused_attached_pkg_fun_linter()
+test_that("box_unused_attached_mod_fun_linter blocks box-attached functions unused.", {
+  linter <- box_unused_attached_mod_fun_linter()
   lint_message_1 <- rex::rex("Imported function unused.")
 
   # filter is unused
   bad_box_usage_1 <- "box::use(
-    fs[dir_ls, path_file],
-    shiny[...],
-    stringr
+    path/to/module_a[a_fun_a, a_fun_b],
+    path/to/module_b[b_fun_b],
   )
 
-  box::use(
-    path/to/module1,
-    path/to/module2[a, b, c],
-    path/to/module3[...]
-  )
-
-  path_file('path/to/file')
-  stringr$str_sub('text', 1, 2)
-  is.reactive(x)
+  a_fun_a()
+  b_fun_b()
   "
 
   lintr::expect_lint(bad_box_usage_1, list(message = lint_message_1), linter)
 })
 
-test_that("box_unused_attached_pkg_fun_linter blocks box-attached aliased functions unused.", {
-  linter <- box_unused_attached_pkg_fun_linter()
+test_that("box_unused_attached_mod_fun_linter blocks box-attached aliased functions unused.", {
+  linter <- box_unused_attached_mod_fun_linter()
   lint_message_1 <- rex::rex("Imported function unused.")
 
   # filter is unused
   bad_box_usage_1 <- "box::use(
-    fs[fun_alias = dir_ls, path_file],
-    shiny[...],
-    stringr
+    path/to/module_a[a_fun_a, fun_alias = a_fun_b],
+    path/to/module_b[b_fun_b],
   )
 
-  box::use(
-    path/to/module1,
-    path/to/module2[a, b, c],
-    path/to/module3[...]
-  )
-
-  path_file('path/to/file')
-  stringr$str_sub('text', 1, 2)
-  is.reactive(x)
+  a_fun_a()
+  b_fun_b()
   "
 
   lintr::expect_lint(bad_box_usage_1, list(message = lint_message_1), linter)
