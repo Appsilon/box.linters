1db7c709daae00229ba6c2a5a7d787d0fdbf6764
Rodrigo Basa <rodrigo@appsilon.com>
2024-05-03 16:07:10 +0800

    include object checking in unused_declared_func_linter

--- R/unused_declared_func_linter.R
+++ R/unused_declared_func_linter.R
@@ -8,14 +8,18 @@ unused_declared_func_linter <- function() {
 
     xml <- source_expression$full_xml_parsed_content
 
-    fun_assignments <- get_declared_functions(xml)
+    fun_assignments <- get_declared_objects(xml)
+    exported_objects <- get_exported_objects(xml)
     function_calls <- get_function_calls(xml)
+    object_calls <- get_object_calls(xml)
+    local_calls_text <- c(function_calls$text, object_calls$text)
 
     lapply(fun_assignments$xml_nodes, function(fun_assign) {
       fun_assign_text <- xml2::xml_text(fun_assign)
       fun_assign_text <- gsub("[`'\"]", "", fun_assign_text)
 
-      if (!fun_assign_text %in% function_calls$text) {
+      if (!fun_assign_text %in% local_calls_text &
+            !fun_assign_text %in% exported_objects$text) {
         lintr::xml_nodes_to_lints(
           fun_assign,
           source_expression = source_expression,
@@ -25,4 +29,14 @@ unused_declared_func_linter <- function() {
       }
     })
   })
+}
+
+get_exported_objects <- function(xml) {
+  xpath_exported_objects <- "
+  //COMMENT[text() = \"#' @export\"]
+  /following-sibling::expr[1]
+  /expr/SYMBOL
+  "
+
+  extract_xml_and_text(xml, xpath_exported_objects)
 }
--- tests/testthat/test-unused_declared_func_linter.R
+++ tests/testthat/test-unused_declared_func_linter.R
@@ -1,3 +1,11 @@
+code_to_xml_expr <- function(text_code) {
+  xml2::read_xml(
+    xmlparsedata::xml_parse_data(
+      parse(text = text_code, keep.source = TRUE)
+    )
+  )
+}
+
 test_that("unused_declared_func_linter skips used declared functions", {
   linter <- unused_declared_func_linter()
 
@@ -43,4 +51,146 @@ test_that("unused_declared_func_linter blocks unused declared functions", {
   lintr::expect_lint(bad_box_usage_1, list(message = lint_message), linter)
   lintr::expect_lint(bad_box_usage_2, list(message = lint_message), linter)
   lintr::expect_lint(bad_box_usage_3, list(message = lint_message), linter)
+})
+
+test_that("get_exported_objects returns correct list of exported objects", {
+  code <- "
+  #' @export
+  fun_a <- function() {
+
+  }
+
+  #' @export
+  fun_b <- function() {
+
+  }
+
+  fun_c <- function() {
+
+  }
+
+  #' @export
+  data_a <- 1
+
+  #' @export
+  data_b <- 2
+
+  data_c <- 3
+  "
+
+  xml_exported_objects <- code_to_xml_expr(code)
+  result <- get_exported_objects(xml_exported_objects)
+  expected_result <- c("fun_a", "fun_b", "data_a", "data_b")
+
+  expect_equal(result$text, expected_result)
+})
+
+test_that("unused_declared_func_linter skips exported functions", {
+  linter <- unused_declared_func_linter()
+
+  code <- "
+  #' @export
+  fun_a <- function() {
+
+  }
+
+  #' @export
+  fun_b <- function() {
+    fun_c()
+  }
+
+  fun_c <- function() {
+
+  }
+  "
+
+  lintr::expect_lint(code, NULL, linter)
+})
+
+test_that("unused_declared_func_linter blocks not locally used private functions", {
+  linter <- unused_declared_func_linter()
+  lint_message <- rex::rex("Declared function unused.")
+
+  code <- "
+  #' @export
+  fun_a <- function() {
+
+  }
+
+  #' @export
+  fun_b <- function() {
+
+  }
+
+  fun_c <- function() {
+
+  }
+  "
+
+  lintr::expect_lint(code, list(message = lint_message), linter)
+})
+
+
+
+
+
+test_that("unused_declared_func_linter skips exported data objects", {
+  linter <- unused_declared_func_linter()
+
+  code <- "
+  #' @export
+  fun_a <- function() {
+
+  }
+
+  #' @export
+  fun_b <- function() {
+    fun_c()
+  }
+
+  fun_c <- function() {
+    obj_c
+  }
+
+  #' @export
+  obj_a <- 1
+
+  #' @export
+  obj_b <- 2
+
+  obj_c <- 3
+  "
+
+  lintr::expect_lint(code, NULL, linter)
+})
+
+test_that("unused_declared_func_linter not locally used private data objects", {
+  linter <- unused_declared_func_linter()
+  lint_message <- rex::rex("Declared function unused.")
+
+  code <- "
+  #' @export
+  fun_a <- function() {
+
+  }
+
+  #' @export
+  fun_b <- function() {
+    fun_c()
+  }
+
+  fun_c <- function() {
+
+  }
+
+  #' @export
+  obj_a <- 1
+
+  #' @export
+  obj_b <- 2
+
+  obj_c <- 3
+  "
+
+  lintr::expect_lint(code, list(message = lint_message), linter)
 })
