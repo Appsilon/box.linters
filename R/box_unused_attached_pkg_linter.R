26dd8e6d2616d11fac69aab87760fed6415ff1a7
Rodrigo Basa <rodrigo@appsilon.com>
2024-05-06 14:26:27 +0800

    box_unused_attached_*_linter documentation. issues #51 #52

--- R/box_unused_attached_pkg_linter.R
+++ R/box_unused_attached_pkg_linter.R
@@ -1,4 +1,67 @@
 #' `box` library unused attached package linter
+#'
+#' Checks that all attached packages are used within the source file. This also covers packages
+#' attached using the `...`.
+#'
+#' @return A custom linter function for use with `r-lib/lintr`.
+#'
+#' @examples
+#' # will produce lints
+#' code <- "
+#' box::use(
+#'   stringr
+#' )
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
+#' code <- "
+#' box::use(
+#'   alias = stringr
+#' )
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
+#' code <- "
+#' box::use(
+#'   path/to/stringr[...]
+#' )
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
+#' # okay
+#' code <- "
+#' box::use(
+#'   stringr
+#' )
+#'
+#' stringr$str_pad()
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
+#' code <- "
+#' box::use(
+#'   alias = stringr
+#' )
+#'
+#' alias$str_pad()
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
+#' code <- "
+#' box::use(
+#'   stringr[...]
+#' )
+#'
+#' str_pad()
+#' "
+#'
+#' lintr::lint(text = code, linters = box_unused_attached_pkg_linter())
+#'
 #' @export
 box_unused_attached_pkg_linter <- function() {
   lintr::Linter(function(source_expression) {
