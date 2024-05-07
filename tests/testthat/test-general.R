b95037513e5069f56bd257495e97592aee03800a
Rodrigo Basa <rodrigo@appsilon.com>
2024-05-07 11:41:39 +0800

    add test for native pipe `|>`

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -12,3 +12,18 @@ mtcars %>%
 
   lintr::expect_lint(code, NULL, linters)
 })
+
+test_that("Should skip native pipe `|>` operator", {
+  linters <- lintr::linters_with_defaults(defaults = box.linters::rhino_default_linters)
+
+  code <- "
+box::use(
+  dplyr[filter, select],
+)
+
+mtcars |>
+  select(mpg, cyl) |>
+  filter(mpg <= 10)"
+
+lintr::expect_lint(code, NULL, linters)
+})
