1e9648ab24d6f644d4c6ea59acc754577cb7434e
Rodrigo Basa <rodrigo@appsilon.com>
2024-07-04 17:44:55 +0800

    sort packages and modules

--- R/style_box_use.R
+++ R/style_box_use.R
@@ -15,9 +15,12 @@ style_box_use_file <- function(filename, run_styler = FALSE) {
 #' @export
 style_box_use_text <- function(text) {
   tree_root <- ts_root(text)
+
   ts_pkgs <- ts_find_all(tree_root, ts_query_pkg)
+  sorted_pkgs <- sort_mod_pkg_calls(ts_pkgs, "pkg")
 
   ts_mods <- ts_find_all(tree_root, ts_query_mod)
+  sorted_mods <- sort_mod_pkg_calls(ts_mods, "mod")
 }
 
 #' @keywords internal
@@ -31,4 +34,53 @@ ts_root <- function(source_text) {
 ts_find_all <- function(tree, query) {
   query <- treesitter::query(treesitter.r::language(), query)
   treesitter::query_matches(query, tree)
+}
+
+#' @keywords internal
+get_nodes_text_by_type <- function(
+    items,
+    type = c(
+      "comment",
+      "full_path",
+      "func_call",
+      "func_name",
+      "mod_call",
+      "pkg_call",
+      "pkg_name"
+    )
+) {
+  type <- match.arg(type)
+  results <- lapply(items[[1]], function(item) {
+    idx_pkg <- match(type, item$name)
+    if (treesitter::is_node(item$node[[idx_pkg]])) {
+      treesitter::node_text(item$node[[idx_pkg]])
+    } else {
+      ""
+    }
+  })
+
+  unlist(results)
+}
+
+#' @keywords internal
+sort_mod_pkg_calls <- function(tree_matches, pkg_or_mod = c("mod", "pkg")) {
+  pkg_or_mod <- match.arg(pkg_or_mod)
+  switch (pkg_or_mod,
+          "mod" = {
+            node_names <- "full_path"
+            node_calls <- "mod_call"
+          },
+          "pkg" = {
+            node_names <- "pkg_name"
+            node_calls <- "pkg_call"
+          }
+  )
+
+  attached_names <- get_nodes_text_by_type(tree_matches, node_names)
+  order_attached_names <- order(attached_names)
+  attached_calls <- get_nodes_text_by_type(tree_matches, node_calls)
+  comments <- get_nodes_text_by_type(tree_matches, "comment")
+  names(attached_calls) <- comments
+
+  attached_calls[order_attached_names]
 }
--- tests/testthat/test-style_box_use.R
+++ tests/testthat/test-style_box_use.R
@@ -10,6 +10,8 @@ box::use(
   testthat::expect_true(treesitter::is_node(ts_tree))
 })
 
+##### ts_find_all #####
+
 test_that("ts_find_all(ts_query_pkg) returns box::use packages", {
   query <- ts_query_pkg
 
@@ -19,10 +21,10 @@ box::use(
   tidyr,
 )"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 2
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
 })
 
 test_that("ts_find_all(ts_query_pkg) does not return box::use modules", {
@@ -34,10 +36,10 @@ box::use(
   path/to/module_b,
 )"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 0
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
 })
 
 test_that("ts_find_all(ts_query_pkg) does not return non box::use calls", {
@@ -48,12 +50,11 @@ some_function <- function() {
   1 + 1
 }"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 0
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
 })
-
 
 test_that("ts_find_all(ts_query_mod) returns box::use modules", {
   query <- ts_query_mod
@@ -64,10 +65,10 @@ box::use(
   path/to/module_b,
 )"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 2
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
 })
 
 test_that("ts_find_all(ts_query_mod) does not return box::use packages", {
@@ -79,11 +80,12 @@ box::use(
   tidyr,
 )"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 0
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
 })
+
 test_that("ts_find_all(ts_query_mod) does not return non box::use calls", {
   query <- ts_query_mod
 
@@ -92,8 +94,346 @@ some_function <- function() {
   1 + 1
 }"
 
-  pkg_nodes <- ts_find_all(ts_root(code), query)
+  matches <- ts_find_all(ts_root(code), query)
   qty_pkgs <- 0
 
-  expect_length(pkg_nodes[[1]], qty_pkgs)
+  expect_length(matches[[1]], qty_pkgs)
+})
+
+##### get_nodes_text_by_type() #####
+
+test_that("get_nodes_text_by_type() returns correct results in order as found", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  pkgs <- get_nodes_text_by_type(matches, "pkg_name")
+
+  expected_result <- c("tidyr", "stringr")
+
+  expect_identical(pkgs, expected_result)
+})
+
+test_that("get_nodes_text_by_type() does not return other types", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr,
+  stringr,
+  path/to/module,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  pkgs <- get_nodes_text_by_type(matches, "pkg_name")
+
+  expected_result <- c("tidyr", "stringr")
+
+  expect_identical(pkgs, expected_result)
+})
+
+test_that("get_nodes_text_by_type() returns results from separate box::use() calls", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr,
+  stringr,
+)
+
+box::use(
+  shiny,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  pkgs <- get_nodes_text_by_type(matches, "pkg_name")
+
+  expected_result <- c("tidyr", "stringr", "shiny")
+
+  expect_identical(pkgs, expected_result)
+})
+
+##### sort_mod_pkg_calls #####
+
+test_that("sort_mod_pkg_calls('pkg') returns sorted packages", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr,
+  dplyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c("dplyr", "stringr", "tidyr")
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('pkg') ignores package aliases", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  alias = tidyr,
+  dplyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c("dplyr", "stringr", "alias = tidyr")
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('pkg') does not touch single-line attached functions", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr[pivot_wider, pivot_longer],
+  dplyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c("dplyr", "stringr", "tidyr[pivot_wider, pivot_longer]")
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('pkg') does not touch multi-line attached functions", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr[
+    pivot_wider,
+    pivot_longer
+  ],
+  dplyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c("dplyr", "stringr", "tidyr[\n    pivot_wider,\n    pivot_longer\n  ]")
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('pkg') returns sorted packages with comments as names", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr[...], # nolint
+  dplyr,
+  stringr[...], # nolint
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c("dplyr", "stringr[...]", "tidyr[...]")
+  names(expected_result) <- c("", "# nolint", "# nolint")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('pkg') does not touch multi-line attached functions comments", {
+  query <- ts_query_pkg
+
+  code <- "
+box::use(
+  tidyr[
+    pivot_wider, # nolint
+    pivot_longer
+  ],
+  dplyr,
+  stringr,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_pkgs <- sort_mod_pkg_calls(matches, "pkg")
+
+  expected_result <- c(
+    "dplyr",
+    "stringr",
+    "tidyr[\n    pivot_wider, # nolint\n    pivot_longer\n  ]"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_pkgs, expected_result)
+})
+
+
+
+
+
+test_that("sort_mod_pkg_calls('mod') returns sorted modules", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  path/a/module_b,
+  path/b/module_a,
+  path/a/module_a,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a",
+    "path/a/module_b",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('mod') ignores module aliases", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  alias = path/a/module_b,
+  path/b/module_a,
+  path/a/module_a,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a",
+    "alias = path/a/module_b",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('mod') does not touch single-line attached functions", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  path/a/module_b[func_b, func_a],
+  path/b/module_a,
+  path/a/module_a,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a",
+    "path/a/module_b[func_b, func_a]",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('mod') does not touch multi-line attached functions", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  path/a/module_b[
+    func_b,
+    func_a
+  ],
+  path/b/module_a,
+  path/a/module_a,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a",
+    "path/a/module_b[
+    func_b,
+    func_a
+  ]",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('mod') returns sorted modules with comments as names", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  path/a/module_b[...], # nolint
+  path/b/module_a,
+  path/a/module_a[...], # nolint
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a[...]",
+    "path/a/module_b[...]",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("# nolint", "# nolint", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
+})
+
+test_that("sort_mod_pkg_calls('mod') does not touch multi-line attached functions comments", {
+  query <- ts_query_mod
+
+  code <- "
+box::use(
+  path/a/module_b[
+    func_b, # nolint
+    func_a
+  ],
+  path/b/module_a,
+  path/a/module_a,
+)"
+
+  matches <- ts_find_all(ts_root(code), query)
+  sorted_mods <- sort_mod_pkg_calls(matches, "mod")
+
+  expected_result <- c(
+    "path/a/module_a",
+    "path/a/module_b[
+    func_b, # nolint
+    func_a
+  ]",
+    "path/b/module_a"
+  )
+  names(expected_result) <- c("", "", "")   # comments are used as names
+
+  expect_identical(sorted_mods, expected_result)
 })
