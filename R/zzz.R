9628f3d510fb6bb26582c2f44c6721499c0ce2c2
Rodrigo Basa <rodrigo@appsilon.com>
2024-08-27 10:36:52 +0200

    need double &&

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 r_version_compatibility <- function() {
-  if (as.numeric(R.Version()$major) >= 4 & as.numeric(R.Version()$minor) >= 3.0) {
+  if (as.numeric(R.Version()$major) >= 4 && as.numeric(R.Version()$minor) >= 3.0) {
     namespaced_function_calls()
   } else {
     NULL
